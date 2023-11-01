e4654c0fc6b12d0d17cbb2fe3e6ac202c609a35e
Camila <millanandanet14@gmail.com>
2023-11-01 10:49:09 -0300

    Upload: front-end

--- gasolina-distancia/main.js
+++ gasolina-distancia/main.js
@@ -1,31 +1,3 @@
 function calcularGasolina() {
 
-    // Puxando informações de entrada do formulário
-    let distancia = document.querySelector("#distancia").value;
-    let consumo = document.querySelector("#consumo").value;
-    let preco = document.querySelector("#preco").value;
-
-    // Calculando a quantidade de gasolina necessária
-    let quantidadeGasolina = distancia / consumo;
-
-    // Calculando o custo da viagem
-    let custoViagem = quantidadeGasolina * preco;
-
-    // Exibindo os resultados
-    
-    alert(`
-        Quantidade Gasolina: ${quantidadeGasolina.toFixed(2)}
-        Custo da viagem: R$ ${custoViagem.toFixed(2)}
-    `);
-
-    // Imprimindo resultados no HTML
-    // console.log(`Quantidade de gasolina necessária: ${quantidadeGasolina} litros`);
-    // console.log(`Custo da viagem: R$${custoViagem.toFixed(2)}`);
-
-    // elementoResultadoGasolina = document.querySelector("#resultado_gasolina_litros");
-    // elementoResultadoCusto = document.querySelector("#resultado_custo");
-
-    // elementoResultadoGasolina.innerHTML = quantidadeGasolina;
-    // elementoResultadoCusto.innerHTML = "R$ " + custoViagem.toFixed(2);
-
 }
--- notas-escolares/main.js
+++ notas-escolares/main.js
@@ -1,38 +1,3 @@
 function calcularNota() {
 
-    // Puxando informações de entrada do formulário
-    let notaLinguagens = document.querySelector("#linguagens").value;
-    let notaMatematica = document.querySelector("#matematica").value;
-    let notaHumanas = document.querySelector("#humanas").value;
-    let notaNatureza = document.querySelector("#natureza").value;
-    let notaRedacao = document.querySelector("#redacao").value;
-
-    // Pesos de cada disciplina
-    let pesoLinguagens = 1;
-    let pesoMatematica = 2;
-    let pesoCienciasHumanas = 1;
-    let pesoCienciasNatureza = 4;
-    let pesoRedacao = 2;
-
-    // Cálculo das notas ponderadas
-    let notaPonderadaLinguagens = notaLinguagens * pesoLinguagens;
-    let notaPonderadaMatematica = notaMatematica * pesoMatematica;
-    let notaPonderadaCienciasHumanas = notaHumanas * pesoCienciasHumanas;
-    let notaPonderadaCienciasNatureza = notaNatureza * pesoCienciasNatureza;
-    let notaPonderadaRedacao = notaRedacao * pesoRedacao;
-
-    // Cálculo da nota final
-    let notaFinal = (
-    notaPonderadaLinguagens +
-    notaPonderadaMatematica +
-    notaPonderadaCienciasHumanas +
-    notaPonderadaCienciasNatureza +
-    notaPonderadaRedacao
-    ) / (pesoLinguagens + pesoMatematica + pesoCienciasHumanas + pesoCienciasNatureza + pesoRedacao);
-
-    alert("Nota Final do ENEM: " + notaFinal);
-    
-    // Imprimindo resultados no HTML
-    // elementoResultadoNota = document.querySelector("#resultado_nota");
-    // elementoResultadoNota.innerHTML = notaFinal.toFixed(2);
 }
