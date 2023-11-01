168d9897eed70b662708c4c4542d3fb267c6f17f
jeremiasmarinho <jeremias10@msn.com>
2023-11-01 16:14:38 -0300

    Segundo teste

--- gasolina-distancia/main.js
+++ gasolina-distancia/main.js
@@ -1,3 +1,11 @@
 function calcularGasolina() {
+  let consumo = document.querySelector("#consumo").value;
+  let preco = document.querySelector("#preco").value;
+  let distancia = document.querySelector("#distancia").value;
 
+  let custoViagem = (distancia / consumo) * preco;
+
+  alert("O custo é: R$ " + custoViagem.toFixed(2));
+
+  console.log(custoViagem);
 }
--- notas-escolares/main.js
+++ notas-escolares/main.js
@@ -1,3 +1,25 @@
 function calcularNota() {
+  let linguagens = document.querySelector("#linguagens").value;
+  let matematica = document.querySelector("#matematica").value;
+  let natureza = document.querySelector("#natureza").value;
+  let redacao = document.querySelector("#redacao").value;
 
+  let pesoLiguaguem = 1;
+  let pesoMatematica = 2;
+  let pesoNatureza = 1;
+  let pesoRedacao = 2;
+
+  let notaPonderadaLinguagens = linguagens * pesoLiguaguem;
+  let notaPonderadaMatematica = matematica * pesoMatematica;
+  let notaPonderadaNatureza = natureza * pesoNatureza;
+  let notaPonderadaRedacao = redacao * pesoRedacao;
+
+  let notaTotal =
+    (notaPonderadaLinguagens +
+      notaPonderadaMatematica +
+      notaPonderadaNatureza +
+      notaPonderadaRedacao) /
+    (pesoLiguaguem + pesoMatematica + pesoNatureza + pesoRedacao);
+
+  alert("A nota final é: " + notaTotal).toFixed(2);
 }
